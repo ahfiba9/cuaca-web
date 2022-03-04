ec81745294bed16246e123f42c8743e4d924e7d2
Ahmad Fikri <ahmad.fikri@aymcommerce.com>
2022-03-05 00:02:12 +0800

    update the persistconfig

--- src/App/configureStore.ts
+++ src/App/configureStore.ts
@@ -5,10 +5,12 @@ import {setGlobalStore} from "src/Redux/store";
 import {rootReducer} from "src/Redux/reducers";
 import {persistReducer, persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: autoMergeLevel2,
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
